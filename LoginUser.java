07252521c13e1e066777d1df5ae58df785fbc2f8
iliyashiravand <ishiravand@gmail.com>
2021-05-30 16:56:08 +0430

    Login User

---
+++ LoginUser.java
@@ -0,0 +1,9 @@
+public class LoginUser {
+    String username;
+    String password;
+
+    public LoginUser(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-import com.google.gson.Gson;
+
 
 import java.io.IOException;
 import java.io.Writer;
@@ -6,10 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Gson gson = new Gson();
-        for (int i=1; i<=10; i++) {
-            Writer writer = Files.newBufferedWriter(Paths.get("Level"+i+".json"));
-        }
+    public static void main(String[] args) {
+        SpecialAnimals.Dog dog = new SpecialAnimals.Dog();
     }
 }
