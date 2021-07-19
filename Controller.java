c75099fb8306cfbae692e6a00e030c2a3bcd5537
Iliya Shiravand <85054450+iliyash1381@users.noreply.github.com>
2021-07-19 14:18:21 +0430

    Add files via upload

--- Controller.java
+++ Controller.java
@@ -4,6 +4,8 @@ import LevelDesign.Level;
 import LevelDesign.Map;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -36,8 +38,14 @@ public class Controller implements Initializable
 
     @FXML private Button Storage;
     @FXML private Button Bucket;
+    @FXML private Button Turn;
     private Image image = new Image(getClass().getResourceAsStream("Grass.png"));
     private Image chickenpng = new Image(getClass().getResourceAsStream("Chicken.png"));
+    private Image turkeypng = new Image(getClass().getResourceAsStream("Turkey.png"));
+    private Image buffalopng = new Image(getClass().getResourceAsStream("Buffalo.png"));
+    private Image lionpng = new Image(getClass().getResourceAsStream("Lion.png"));
+    private Image bearpng = new Image(getClass().getResourceAsStream("Bear.png"));
+    private Image tigerpng = new Image(getClass().getResourceAsStream("Tiger.png"));
     @FXML
     private GridPane Map;
 
@@ -63,6 +71,16 @@ public class Controller implements Initializable
                 }
             }
             reader.close();
+        }
+        else{
+            Level level = new Level();
+            level = manager.readBasicLevelInfo(level,1);
+            manager.mapInitialize(level);
+            level.playerName  = username;
+            level.levelStarted = false;
+            playerLevel = level;
+            levels.add(level);
+            saveLevel();
         }
     }
 
@@ -98,18 +116,21 @@ public class Controller implements Initializable
             // manager.Well(playerLevel);
             System.out.println(playerLevel.bucket.capacity);
         });
-
+        Tile[][] tiles  = new Tile[6][6];
         for (int i=0; i <6; i++){
             for (int j=0; j<6; j++){
                 Tile tile = new Tile(i,j);
+                tiles[i][j] = tile;
                 final int x = i+1;
                 final int y = j+1;
+                //////////grass plant
                 tile.grass.setOnMouseClicked(mouseEvent -> {
                     try {
                         readLevel();
-                        manager.Plant(y,x,playerLevel);
+                        manager.Plant(x,y,playerLevel);
                         System.out.println(playerLevel.map.map[x-1][y-1].grass);
                         if (playerLevel.map.map[x-1][y-1].grass>=1){
+                            tile.grassLabel.setText(playerLevel.map.map[x-1][y-1].grass+"");
                             tile.grass.setImage(image);
                         }
                         saveLevel();
@@ -119,46 +140,119 @@ public class Controller implements Initializable
                     }
 
                 });
-                try {
-                    readLevel();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                tile.grassLabel.setText("");
-                if (playerLevel.map.map[x-1][y-1].grass>=1){
-                    tile.grass.setImage(image);
-                    tile.grassLabel.setText(playerLevel.map.map[x-1][y-1].grass+"");
-                    System.out.println(872);
-                }
-                tile.cCounter.setText("");
-                tile.tCounter.setText("");
-                tile.bCounter.setText("");
-                int r=0;
-                for (int t=0 ; t<playerLevel.chickens.size(); t++){
-                    if (playerLevel.chickens.get(t).x == x && playerLevel.chickens.get(t).y == y){
-                        tile.turkey.setImage(chickenpng);
-                        r++;
-                    }
-                    if (r!=0) {
-                        tile.cCounter.setText(r + "");
-                        r=0;
-                    }
-                }
                 tile.grassLabel.setOnMouseClicked(mouseEvent -> {
                     try {
                         readLevel();
-                        manager.Plant(y,x,playerLevel);
-                        //System.out.println(playerLevel.map.map[x-1][y-1].grass);
+                        manager.Plant(x,y,playerLevel);
+                        if (playerLevel.map.map[x-1][y-1].grass>=1){
+                            tile.grassLabel.setText(playerLevel.map.map[x-1][y-1].grass+"");
+                            tile.grass.setImage(image);
+                        }
                         saveLevel();
-                        System.out.println(playerLevel.bucket.capacity);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
 
                 });
+                try {
+                    readLevel();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (playerLevel.map.map[x-1][y-1].grass==0){
+                    tile.grass.setImage(null);
+                    tile.grassLabel.setText("");
+                }
 
-                    ArrayList<String> s = new ArrayList<>();
-                    s.add("ingredients");
+                ///////Domestic Animals
+                tile.cCounter.setText("");
+                tile.tCounter.setText("");
+                tile.bCounter.setText("");
+                int r=0;
+                for (int t=0 ; t<playerLevel.chickens.size(); t++) {
+                    if (playerLevel.chickens.get(t).x == x && playerLevel.chickens.get(t).y == y) {
+                        tile.turkey.setImage(chickenpng);
+                        r++;
+                    }
+                }
+                if (r!=0) {
+                        tile.cCounter.setText(r + "");
+                        r=0;
+                }
+
+                r=0;
+                for (int t=0 ; t<playerLevel.turkies.size(); t++) {
+                    if (playerLevel.turkies.get(t).x == x && playerLevel.turkies.get(t).y == y) {
+                        tile.chicken.setImage(turkeypng);
+                        r++;
+                    }
+                }
+                if (r!=0) {
+                        tile.tCounter.setText(r + "");
+                        r=0;
+                }
+
+                r=0;
+                for (int t=0 ; t<playerLevel.buffalos.size(); t++) {
+                    if (playerLevel.buffalos.get(t).x == x && playerLevel.buffalos.get(t).y == y) {
+                        tile.buffalo.setImage(buffalopng);
+                        r++;
+                    }
+                }
+                if (r!=0) {
+                        tile.bCounter.setText(r + "");
+                        r=0;
+                }
+
+                //////Wild Animals
+
+                tile.cCounter.setText("");
+                tile.tCounter.setText("");
+                tile.bCounter.setText("");
+                for (int t=0 ; t<playerLevel.lions.size(); t++) {
+                    if (playerLevel.lions.get(t).x == x && playerLevel.lions.get(t).y == y) {
+                        tile.turkey.setImage(lionpng);
+                        r++;
+                    }
+                }
+                if (r!=0) {
+                    tile.cCounter.setText(r + "");
+                    r=0;
+                }
+
+                r=0;
+                for (int t=0 ; t<playerLevel.bears.size(); t++) {
+                    if (playerLevel.bears.get(t).x == x && playerLevel.bears.get(t).y == y) {
+                        tile.chicken.setImage(bearpng);
+                        r++;
+                    }
+                }
+                if (r!=0) {
+                    tile.tCounter.setText(r + "");
+                    r=0;
+                }
+
+                r=0;
+                for (int t=0 ; t<playerLevel.tigers.size(); t++) {
+                    if (playerLevel.tigers.get(t).x == x && playerLevel.tigers.get(t).y == y) {
+                        tile.buffalo.setImage(tigerpng);
+                        r++;
+                    }
+                }
+                if (r!=0) {
+                    tile.bCounter.setText(r + "");
+                    r=0;
+                }
+
+                ////////
+
+
+
+
+
+                ////////// ChoiceBox
+                ArrayList<String> s = new ArrayList<>();
+                s.add("ingredients");
                 try {
                     readLevel();
                 } catch (IOException e) {
@@ -166,10 +260,10 @@ public class Controller implements Initializable
                 }
                 for (int t=0 ; t<playerLevel.ingredients.size();t++){
                             if (playerLevel.ingredients.get(t).x==x && playerLevel.ingredients.get(t).y==y){
-                                s.add(playerLevel.ingredients.get(t).name);
+                                s.add(playerLevel.ingredients.get(t).name + "   " + playerLevel.ingredients.get(t).expire);
                             }
                         }
-                tile.ingredient.setValue("Ingredients");
+                tile.ingredient.setValue("ingredients");
                 tile.ingredient.getItems().addAll(s);
                 s.clear();
                 tile.ingredient.getItems().addAll(s);
@@ -188,10 +282,167 @@ public class Controller implements Initializable
                 }
 
 
+                //////
+
+
                 Map.add(tile, j,i);
             }
         }
+        Turn.setOnAction(actionEvent -> {
+            try {
+                readLevel();
+                manager.turn(playerLevel, 1);
+                ////Animals & ChoiceBox
+                for (int i=0 ; i<6;i++){
+                    for (int j=0 ; j<6 ;j++){
+                        final int x = i+1;
+                        final int y = j+1;
+                        Tile tile = tiles[i][j];
+                        //grass
+                        if (playerLevel.map.map[x-1][y-1].grass==0){
+                            tile.grass.setImage(null);
+                            tile.grassLabel.setText("");
+                        }
+                        else{
+                            tile.grass.setImage(image);
+                            tile.grassLabel.setText(playerLevel.map.map[x-1][y-1].grass+"");
+                        }
 
+                        ///Animal
+                        tile.cCounter.setText("");
+                        tile.tCounter.setText("");
+                        tile.bCounter.setText("");
+
+                        int r=0;
+                        int a = 0;
+                        for (int t=0 ; t<playerLevel.chickens.size(); t++) {
+                            if (playerLevel.chickens.get(t).x == x && playerLevel.chickens.get(t).y == y) {
+                                tile.turkey.setImage(chickenpng);
+                                r++;
+                                a=1;
+                            }
+
+                        }
+                        if (a==0){
+                            tile.turkey.setImage(null);
+                        }
+                        a=0;
+                        if (r!=0) {
+                            tile.cCounter.setText(r + "");
+                            r=0;
+                        }
+
+                        r=0;
+                        for (int t=0 ; t<playerLevel.turkies.size(); t++) {
+                            if (playerLevel.turkies.get(t).x == x && playerLevel.turkies.get(t).y == y) {
+                                tile.chicken.setImage(turkeypng);
+                                r++;
+                                a=1;
+                            }
+                        }
+                        if (a==0){
+                            tile.chicken.setImage(null);
+                        }
+                        a=0;
+                        if (r!=0) {
+                            tile.tCounter.setText(r + "");
+                            r=0;
+                        }
+
+                        r=0;
+                        for (int t=0 ; t<playerLevel.buffalos.size(); t++) {
+                            if (playerLevel.buffalos.get(t).x == x && playerLevel.buffalos.get(t).y == y) {
+                                tile.buffalo.setImage(buffalopng);
+                                r++;
+                                a=1;
+                            }
+                        }
+                        if (r!=0) {
+                            tile.bCounter.setText(r + "");
+                            r=0;
+                        }
+                        if (a==0){
+                            tile.buffalo.setImage(null);
+                        }
+                        a=0;
+                        for (int t=0 ; t<playerLevel.lions.size(); t++) {
+                            if (playerLevel.lions.get(t).x == x && playerLevel.lions.get(t).y == y) {
+                                tile.turkey.setImage(lionpng);
+                                r++;
+                            }
+                        }
+                        if (r!=0) {
+                            tile.cCounter.setText(r + "");
+                            r=0;
+                        }
+
+                        r=0;
+                        for (int t=0 ; t<playerLevel.bears.size(); t++) {
+                            if (playerLevel.bears.get(t).x == x && playerLevel.bears.get(t).y == y) {
+                                tile.chicken.setImage(bearpng);
+                                r++;
+                            }
+                        }
+                        if (r!=0) {
+                            tile.tCounter.setText(r + "");
+                            r=0;
+                        }
+
+                        r=0;
+                        for (int t=0 ; t<playerLevel.tigers.size(); t++) {
+                            if (playerLevel.tigers.get(t).x == x && playerLevel.tigers.get(t).y == y) {
+                                tile.buffalo.setImage(tigerpng);
+                                r++;
+                            }
+                        }
+                        if (r!=0) {
+                            tile.bCounter.setText(r + "");
+                            r=0;
+                        }
+
+                        //ChoiceBox
+
+
+                        ArrayList<String> s = new ArrayList<>();
+                        s.add("ingredients");
+                        tile.ingredient.getItems().removeAll();
+                        for (int t=0 ; t<playerLevel.ingredients.size();t++){
+                            if (playerLevel.ingredients.get(t).x==x && playerLevel.ingredients.get(t).y==y){
+                                s.add(playerLevel.ingredients.get(t).name + "   " + playerLevel.ingredients.get(t).expire);
+                            }
+                        }
+                        tile.ingredient.getItems().addAll(s);
+                        tile.ingredient.setValue("ingredients");
+                        s.clear();
+
+                        s.add("Animals");
+                        tile.animal.getItems().removeAll();
+                        for (int t=0 ; t<playerLevel.chickens.size();t++){
+                            if (playerLevel.chickens.get(t).x==x && playerLevel.chickens.get(t).y==y){
+                                s.add("Chicken    "+ playerLevel.chickens.get(t).health);
+                                System.out.println(s.get(1));
+                            }
+                        }
+                        tile.animal.getItems().addAll(s);
+                        tile.animal.setValue("Animals");
+                        try {
+                            saveLevel();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+
+                }
+                /////
+
+
+                saveLevel();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
 }
